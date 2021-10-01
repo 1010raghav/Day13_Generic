c3de094f9ec4a07f34697fee8bc2d1efc9509004
Raghav <raghavwandre1234@gmail.com>
2021-10-01 16:02:00 +0530

    UC2 Compare 3 Floats

--- Program.cs
+++ Program.cs
@@ -10,9 +10,8 @@ namespace Day13_practice
         {
             Console.WriteLine("Find Max Value");
 
-           int output = UC1.Maxint(11,22,33);
-           Console.WriteLine(output);     
+           double doubleoutput = UC1.Maxint(11.2, 22.1, 33.23);
+           Console.WriteLine(doubleoutput);    
         }
     }
 }
-
