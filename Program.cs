3042cd2430370b27fbd64ebee04dd78bba9702a6
Raghav <raghavwandre1234@gmail.com>
2021-10-01 15:56:47 +0530

    Program file for UC1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Day13_practice
+{
+    class Program
+    {
+        public static object TC1Integer { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Value");
+
+           int output = UC1.Maxint(11,22,33);
+           Console.WriteLine(output);     
+        }
+    }
+}
+
